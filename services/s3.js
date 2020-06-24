2df16ab45d83deceb580f50ce0af7dafe04606ff
Deepanshu Gupta <deepanshu@Deepanshus-MacBook-Pro.local>
2020-06-23 21:15:23 -0700

    stuff

--- services/s3.js
+++ services/s3.js
@@ -1,22 +1,24 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-  accessKeyId: 'AKIAU4LAKYYRWTTMUTTH',
-  secretAccessKey: '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'
+  accessKeyId: "AKIAU4LAKYYRWTTMUTTH",
+  secretAccessKey: "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"
 });
 
 const putObject = (fileName, data) => {
-  const data = new Buffer(data, 'binary');
-  s3.putObject({
-    Bucket: 'db75b70c-f6a0-4144-9c87-c2bb80f34f6c',
-    Key: fileName,
-    Body: data,
-    ACL: 'public-read'
-  },function (resp) {
-    console.log('Successfully uploaded package.', fileName);
-  });
-}
-
+  const putData = new Buffer(data, "binary");
+  s3.putObject(
+    {
+      Bucket: "db75b70c-f6a0-4144-9c87-c2bb80f34f6c",
+      Key: fileName,
+      Body: putData,
+      ACL: "public-read"
+    },
+    function(resp) {
+      console.log("Successfully uploaded package.", fileName);
+    }
+  );
+};
 
 module.exports = {
   putObject
